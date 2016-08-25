73ea853eecacbdfd536966d2bee5715b3539915d
MMC <maksemuz@gmail.com>
2016-08-25 16:55:11 +0300

    replace commented fragments

--- movies.rb
+++ movies.rb
@@ -4,20 +4,25 @@
 if ARGV.length == 0
   puts "You did not enter filename. Using movies.txt"
   movie_file = "movies.txt"
-elsif File.exist?(ARGV[0])
-    movie_file = ARGV[0]
 else
-  puts "File #{ARGV[0]} does not exist. Using movies.txt"
-  movie_file = "movies.txt"
+    movie_file = ARGV[0]
+end
+if !File.exist?(movie_file)
+  puts "File #{ARGV[0]} does not exist. Nothing to process. Exit."
+  exit
 end
 
 f = File.readlines(movie_file)
-for line in f do
+f.each do |line|
   line.force_encoding(Encoding::UTF_8)
   amovie = line.split("|")
   stars = amovie[7].to_s.split(".")
-  stars_out = Array.new(stars[1].to_i,"*")
+  if stars[0].to_i > 8
+    i = stars[1].to_i + 10
+  else
+    i = stars[1].to_i
+  end
   if amovie[1].include?("Time")
-      puts "#{amovie[1]} #{stars_out.join}"
+      puts "#{amovie[1]} #{"*" * i}"
   end
 end
