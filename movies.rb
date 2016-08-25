3be5b171dd5c85c49241cc74c14f674ef946921c
MMC <maksemuz@gmail.com>
2016-08-25 12:24:01 +0300

    add filename workout

--- movies.rb
+++ movies.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
-f = File.readlines("movies.txt")
+
+
+if ARGV.length == 0
+  puts "You did not enter filename. Using movies.txt"
+  movie_file = "movies.txt"
+elsif File.exist?(ARGV[0])
+    movie_file = ARGV[0]
+else
+  puts "File #{ARGV[0]} does not exist. Using movies.txt"
+  movie_file = "movies.txt"
+end
+puts movie_file
+f = File.readlines(movie_file)
 for line in f do
   line.force_encoding(Encoding::UTF_8)
   amovie = line.split("|")
