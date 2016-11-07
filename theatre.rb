5cf3b02409fde56da353f310ff9971a133fe9237
MMC <maksemuz@gmail.com>
2016-11-07 09:56:22 +0300

    SCHEDULE is modified Theatre.show is corrected Theatre.when is corrected Comments are added

--- theatre.rb
+++ theatre.rb
@@ -24,7 +24,7 @@ class Theatre < MovieCollection
   # this method shows time when you can watch the movie
   def when?(movie_name)
     movie = self.all.find { |val| val.title == movie_name}
-    time = SCHEDULE.find { |key,val| puts val; key if movie.matches?(val.to_a.flatten[0],val.to_a.flatten[1])}
+    time = SCHEDULE.find { |key,val| key if movie.matches?(val.to_a.flatten[0],val.to_a.flatten[1])}
     raise ArgumentError,"You cannot watch \"#{movie.title}\" in our theatre, it does not match our schedule filters." unless time
     "#{movie_name}. You can watch it at #{time[0].first}:00 - #{time[0].last}:00"
   end
