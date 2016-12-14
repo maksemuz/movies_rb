c2e58e0be1a162d142fffae00ae6e71b7019b6d3
MMC <maksemuz@gmail.com>
2016-12-14 20:28:53 +0300

    private methods are moved to the end

--- netflix.rb
+++ netflix.rb
@@ -22,19 +22,6 @@ module Kino
       @user_account.format
     end
 
-    private def plain_filters?(filters, &block)
-      all.first.methods.include?(filters.keys.first) || block
-    end
-
-    private def saved_searches?(searches)
-      case searches
-        when Symbol
-          @requests[searches]
-        when Hash
-          searches.size == 1 && @requests.key?(searches.keys.first)
-      end
-    end
-
     def show(parameters = {}, &block)
       films = if plain_filters?(parameters, &block)
         filter(parameters)
@@ -46,22 +33,6 @@ module Kino
       raise ArgumentError, 'Nothing matches your request,'\
         ' please correct it' if films.nil?
       get_film(get_rnd_film(films))
-    end
-
-
-    private def get_film(to_watch)
-      dur = to_watch.duration
-      start_time = Time.now
-      end_time = start_time + (dur * 60)
-      raise ArgumentError, "There is not enough money to watch #{to_watch.title}."\
-        "Your balance is #{@user_account.format}." \
-        unless (@user_account - to_watch.price) >= Money.new(0, 'USD')
-      @user_account -= to_watch.price
-      puts "Now showing: #{to_watch.long_title}."\
-        "#{start_time.strftime('%H:%M')} - #{end_time.strftime('%H:%M')}"
-      puts "Price is #{to_watch.price.format}."\
-        "Now your balance is: #{Money.new(@user_account, 'USD').format}"
-      to_watch
     end
 
     def how_much?(movie_name)
@@ -80,6 +51,34 @@ module Kino
       else
         @requests[name] = filter
       end
+    end
+
+    private def plain_filters?(filters, &block)
+      all.first.methods.include?(filters.keys.first) || block
+    end
+
+    private def saved_searches?(searches)
+      case searches
+        when Symbol
+          @requests[searches]
+        when Hash
+          searches.size == 1 && @requests.key?(searches.keys.first)
+      end
+    end
+
+    private def get_film(to_watch)
+      dur = to_watch.duration
+      start_time = Time.now
+      end_time = start_time + (dur * 60)
+      raise ArgumentError, "There is not enough money to watch #{to_watch.title}."\
+        "Your balance is #{@user_account.format}." \
+        unless (@user_account - to_watch.price) >= Money.new(0, 'USD')
+      @user_account -= to_watch.price
+      puts "Now showing: #{to_watch.long_title}."\
+        "#{start_time.strftime('%H:%M')} - #{end_time.strftime('%H:%M')}"
+      puts "Price is #{to_watch.price.format}."\
+        "Now your balance is: #{Money.new(@user_account, 'USD').format}"
+      to_watch
     end
 
   end
